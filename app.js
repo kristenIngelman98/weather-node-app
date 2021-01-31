e0ef3078ecdc4b225045e185126a77f2b5a5686f
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-01-31 12:59:38 -0800

    used mapbox.com API to get the latitude and longitude of Washington State

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ request({ url: url, json: true }, (error, response) => {
     const weather_description = response.body.current.weather_descriptions[0];
     const temperature = response.body.current.temperature;
     const feelslike = response.body.current.feelslike;
-    // console.log(response.body.current)
     console.log(weather_description + ". It is currently " + temperature + " degrees celcius and feels like " + feelslike + " degrees out.");
 })
 
+// Goal: Print the lat/long for Washington
+
+// 1. Fire off a new request to the URL explore in the browser
+// 2. Have the request module parse it as JSON
+// 3. Print both the lat and long to the terminal
+
+const url_mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Washington.json?limit=2&access_token=pk.eyJ1Ijoia3Jpc3Rlbi1pbmdlbG1hbiIsImEiOiJja2tsbHY2djIxY2YxMm9wN3Q3NHNlMXNlIn0.MNajsWvAkI0TWRbdzJptNA';
+
+request({ url: url_mapbox, json: true }, (error, response) => {
+    const data = response.body.features[0].center[0];
+    const state = response.body.features[0].place_name;
+    const lat = response.body.features[0].center[0];
+    const long = response.body.features[0].center[1];
+  
+    console.log(state + " is located at the coordinates: " + lat +  " ," + long);
+})
