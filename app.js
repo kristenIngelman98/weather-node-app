5f6df685f7f1657a08c900364236e937c15a5d49
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-01-31 12:34:29 -0800

    grabbed data from the weatherstack API and printed to the console

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const url = 'http://api.weatherstack.com/current?access_key=140bb9a7b88454a8f673
 // requests takes 2 arguments
 // json: true --> we would like a response to be in json
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.current)
+    const weather_description = response.body.current.weather_descriptions[0];
+    const temperature = response.body.current.temperature;
+    const feelslike = response.body.current.feelslike;
+    // console.log(response.body.current)
+    console.log(weather_description + ". It is currently " + temperature + " degrees celcius and feels like " + feelslike + " degrees out.");
 })
 
