58847a0eda56d37b0a1f68bed79ade4c1a1bec97
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-01-31 12:22:37 -0800

    connected weatherstack API

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=140bb9a7b88454a8f6731d21343d15f7&query=37.8267,-122.4233';
+
+// requests takes 2 arguments
+// json: true --> we would like a response to be in json
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current)
+})
+
